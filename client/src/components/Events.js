2b98728749ecc5e9834364ae0b721b8b44062ffa
philemonheng <phil.hsy@outlook.com>
2021-05-25 20:47:19 +0800

    edit events

--- client/src/components/Events.js
+++ client/src/components/Events.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
 import TitleText from '../components-reusable/TitleText'
 
 import {
@@ -17,36 +19,17 @@ const eventsData = [
   {
     page: 'home',
     section: 'events',
-    title: 'Church Retreat 2019',
-    text: 'Pellentesque habitant morbi tristique senectus et. Pellentesque habitant morbi. Ntesque habitant morbi tristique sene.',
+    title: 'TRBC - TRBK Carnival',
+    text: `Pellentesque habitant morbi tristique senectus et. Pellentesque habitant morbi. habitant morbi tristique sene. Pellentesque habitant morbi tristique senectus et. Ntesque habitant morbi tristique sene. Pellentesque habitant morbi tristique senectus et. Ntesque habitant morbi tristique sene. Pellentesque habitant morbi tristique senectus et.
+    Ntesque habitant morbi tristique sene. Pellentesque habitant morbi tristique senectus et. Ntesque habitant morbi tristique sene. Pellentesque habitant morbi tristique senectus et. Ntesque habitant morbi tristique sene. Pellentesque habitant morbi tristique senectus et. Ntesque habitant morbi tristique sene. Pellentesque habitant morbi tristique senectus et.`,
     date: '21-25 May 2021',
   },
   {
     page: 'home',
     section: 'events',
-    title: 'Church Retreat 2019',
-    text: 'Pellentesque habitant morbi tristique senectus et. Pellentesque habitant morbi. Ntesque habitant morbi tristique sene.',
-    date: '21-25 May 2021',
-  },
-  {
-    page: 'home',
-    section: 'events',
-    title: 'Church Retreat 2019',
-    text: 'Pellentesque habitant morbi tristique senectus et. Pellentesque habitant morbi. Ntesque habitant morbi tristique sene.',
-    date: '21-25 May 2021',
-  },
-  {
-    page: 'home',
-    section: 'events',
-    title: 'Church Retreat 2019',
-    text: 'Pellentesque habitant morbi tristique senectus et. Pellentesque habitant morbi. Ntesque habitant morbi tristique sene.',
-    date: '21-25 May 2021',
-  },
-  {
-    page: 'home',
-    section: 'events',
-    title: 'Church Retreat 2019',
-    text: 'Pellentesque habitant morbi tristique senectus et. Pellentesque habitant morbi. Ntesque habitant morbi tristique sene.',
+    title: 'Combined Church Camp',
+    text: `Pellentesque habitant morbi tristique senectus et. Pellentesque habitant morbi. habitant morbi tristique sene. Pellentesque habitant morbi tristique senectus et. Pellentesque habitant morbi tristique senectus et. Ntesque habitant morbi tristique sene. Pellentesque habitant morbi tristique senectus et.
+    Ntesque habitant morbi tristique sene. Pellentesque habitant morbi tristique senectus et.`,
     date: '21-25 May 2021',
   },
 ]
@@ -56,9 +39,14 @@ const cardStyle = {
   flexDirection: 'column',
   textAlign: 'left',
   margin: '20px',
-  maxWidth: '320px',
+  maxWidth: '550px',
   border: 'none',
   backgroundColor: cardColor,
+}
+
+const imgStyle = {
+  maxHeight: '300px',
+  objectFit: 'cover',
 }
 
 const iconStyle = {
@@ -66,22 +54,51 @@ const iconStyle = {
   color: iconColor,
 }
 
+const linkStyle = {
+  wordBreak: 'break-word',
+}
+
+const splitLines = (text) => {
+  const splitText = text.split('\n')
+  const trimmedText = splitText.map(text =>
+    text.trim()
+  )
+  return trimmedText
+}
+
 const Event = ({ event }) => {
-  const { title, text, date } = event
+  const { title, text, date, imgSrc } = event
 
   return (
-    <Card style={cardStyle}>
-      <Title style={{ fontSize: '1.2rem', fontWeight: '700', display: 'block' }}>
+    <Card
+      cover={imgSrc && <img src={imgSrc} style={imgStyle} />}
+      style={cardStyle}
+    >
+      <Title style={{ fontSize: '1.8rem', fontWeight: '700', display: 'block' }}>
         {title}
       </Title>
-      <Paragraph style={{ fontSize: '1rem', fontWeight: '300', margin: '20px 0' }}>{text}</Paragraph>
+      <div>
+        {splitLines(text).map((paragraph, i) =>
+          <Paragraph key={i} style={{ fontSize: '1rem', fontWeight: '300', margin: '20px 0' }}>
+            {(() => {
+              if (paragraph.substring(0, 8) === 'https://') {
+                return <Link href={paragraph} style={linkStyle} target='_blank'>{paragraph}</Link>
+              } else if (paragraph === '') {
+                return <span>&nbsp;</span>
+              } else {
+                return paragraph
+              }
+            })()}
+          </Paragraph>
+        )}
+      </div>
       <Divider />
       <Meta
         style={{ textAlign: 'left' }}
         avatar={<CalendarOutlined style={iconStyle} />}
         description={date}
       />
-    </Card>
+    </Card >
   )
 }
 
