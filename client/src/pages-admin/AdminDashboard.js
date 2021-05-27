a30c65b6719d9659d09404021e473b8905e9fa10
philemonheng <phil.hsy@outlook.com>
2021-05-27 15:17:32 +0800

    set up admin dashboard

--- client/src/pages-admin/AdminDashboard.js
+++ client/src/pages-admin/AdminDashboard.js
@@ -1,44 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 
 import { logUserOut } from '../reducers/loggedInUserReducer'
 import { useSelector } from 'react-redux'
 
 import Notification from '../components-reusable/Notification'
+import AdminDashboardNavBar from '../pages-admin/AdminDashboardNavBar'
+import AdminDashboardContent from '../pages-admin/AdminDashboardContent'
 
-import {
-  colorLOrange,
-  colorPrimaryOrange,
-  colorDGray,
-  colorWhite,
-} from '../helper-files/colors'
-
-import { Layout, Button, Menu, Dropdown, Divider } from 'antd'
-const { Header, Content, Sider } = Layout
-const { SubMenu } = Menu
-
-import { UserOutlined } from '@ant-design/icons'
-
-const loggedInUserMenu = (handleLogout, loggedInUser) => {
-
-  return (
-    <Menu
-      className='logged-in-user-menu'
-      orientation='vertical'
-      style={{ border: 'None' }}
-      subMenuOpenDelay={0}
-    >
-      <Menu.Item disabled>
-        {loggedInUser.username}
-      </Menu.Item>
-      <Divider style={{ margin: '5px 0' }} />
-      <Menu.Item onClick={handleLogout}>
-        Log Out
-      </Menu.Item>
-    </Menu>
-  )
-}
+import { Layout } from 'antd'
+const { Content } = Layout
 
 const AdminDashboard = (props) => {
   const {
@@ -57,33 +28,10 @@ const AdminDashboard = (props) => {
   return (
     <Layout className='admin-dashboard'>
       <Notification notification={notification} />
-      <Header
-        style={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          boxShadow: '0px -2px 5px 0 rgba(0,0,0,0.2)',
-          backgroundColor: colorWhite,
-        }}
-      >
-        <Button
-          style={{ border: 'none', lineHeight: 0, }}
-        >Admin Home
-          <Link replace to='/admin'></Link>
-        </Button>
-        <Dropdown
-          overlay={() => loggedInUserMenu(handleLogout, loggedInUser)}
-          trigger={['click']}
-        >
-          <Button
-            icon={<UserOutlined style={{ fontSize: '25px' }} />}
-            style={{ border: 'none' }}
-          />
-        </Dropdown>
-      </Header>
+      <AdminDashboardNavBar handleLogout={handleLogout} loggedInUser={loggedInUser} />
 
-      <Content>
-
+      <Content style={{ padding: '0 50px 50px 50px' }}>
+        <AdminDashboardContent />
       </Content>
     </Layout>
   )
