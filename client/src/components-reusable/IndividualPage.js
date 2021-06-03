ba3bba63d50cd2164c3773902f758aebfbca7781
philemonheng <phil.hsy@outlook.com>
2021-06-04 00:42:18 +0800

    edit services indiv pages

--- client/src/components-reusable/IndividualPage.js
+++ client/src/components-reusable/IndividualPage.js
@@ -76,7 +76,7 @@ const IndividualPage = ({ item }) => {
   return (
     <>
       <Banner imageTitlePairs={[{
-        title: convertName('dashed', 'proper', item.sectionName),
+        title: item.service ? item.service : convertName('dashed', 'proper', item.sectionName),
         image: exterior
       }]} />
       <Row justify='center' style={rowStyle}>
