23647e6b58a3395696cf9d58b9327cd13e4f5551
philemonheng <phil.hsy@outlook.com>
2021-06-23 17:13:23 +0800

    edit your first visit

--- client/src/pages/ImNew.jsx
+++ client/src/pages/ImNew.jsx
@@ -20,19 +20,17 @@ const titleUnderlineStyle = {
 }
 
 const yourFirstVisitMd = `
-Welcome to Thomson Road Baptist Church and this being your first visit allow us to give you an orientation of our building. We have two blocks consisting of the main building where most of our activities are held and an educational block which houses our kindergarten ministry.
+Welcome to Thomson Road Baptist Church and this being your first visit, allow us to give you an orientation of our building. We have two blocks consisting of the main building where most of our activities are held and an educational block which houses our **kindergarten ministry**.
 
 The main building consist of five floors, our main ministries are located on the first three floors and the second floor of the educational block.
 
-The first floor is where our multi-purpose hall is located and on Sundays we usually will have a Tea Fellowship after the service in this place.
+The first floor is where our **multi-purpose hall** is located and on Sundays we usually will have a Tea Fellowship after the service in this place.
 
-Our main sanctuary is on the 2nd floor.
+Our **main sanctuary** is on the 2nd floor.
 
-Our classrooms, library, choir room and Church Office are located on the 3rd floor.
+Our **classrooms, library, choir room and Church Office** are located on the 3rd floor.
 
-We have a Children Worship Ministry called WonderKids at the Fellowship Hall located on the 3rd floor of the main building.
-
-You can find the restrooms located on all floors.
+You can find the **restrooms** located on all floors.
 
 Please do not hesitate to ask any of us to assist you in finding your way when you visit us.
 `
