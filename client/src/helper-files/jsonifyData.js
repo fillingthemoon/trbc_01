f76242f04b71a57eb7b401c091cddb37e5255428
philemonheng <phil.hsy@outlook.com>
2021-06-23 11:18:10 +0800

    update placement of mjo

--- client/src/helper-files/jsonifyData.js
+++ client/src/helper-files/jsonifyData.js
@@ -9,10 +9,10 @@ const callbackFunc = (err, result) => {
 import announcementsData from '../data/announcements.js'
 import discipleshipData from '../data/discipleship.js'
 import eventsData from '../data/events.js'
+import ministryJobOpeningsData from '../data/ministry-job-openings.js'
 import missionsData from '../data/missions.js'
 import ourHistoryData from '../data/our-history.js'
 import ourTeamData from '../data/our-team.js'
-import ministryJobOpeningsData from '../data/ministry-job-openings.js'
 import outreachData from '../data/outreach.js'
 import servicesData from '../data/services.js'
 import statementOfFaithData from '../data/statement-of-faith.js'
@@ -21,10 +21,10 @@ import upcomingSermonsData from '../data/upcoming-sermons.js'
 const announcementsDataJSON = JSON.stringify(announcementsData, null, 2)
 const discipleshipDataJSON = JSON.stringify(discipleshipData, null, 2)
 const eventsDataJSON = JSON.stringify(eventsData, null, 2)
+const ministryJobOpeningsJSON = JSON.stringify(ministryJobOpeningsData, null, 2)
 const missionsDataJSON = JSON.stringify(missionsData, null, 2)
 const ourHistoryDataJSON = JSON.stringify(ourHistoryData, null, 2)
 const ourTeamDataJSON = JSON.stringify(ourTeamData, null, 2)
-const ministryJobOpeningsJSON = JSON.stringify(ministryJobOpeningsData, null, 2)
 const outreachDataJSON = JSON.stringify(outreachData, null, 2)
 const servicesDataJSON = JSON.stringify(servicesData, null, 2)
 const statementOfFaithDataJSON = JSON.stringify(statementOfFaithData, null, 2)
@@ -40,10 +40,10 @@ const upcomingSermonsDataJSON = JSON.stringify(upcomingSermonsData, null, 2)
 fs.writeFile('../server/data-json/announcements.json', announcementsDataJSON, callbackFunc)
 fs.writeFile('../server/data-json/discipleship.json', discipleshipDataJSON, callbackFunc)
 fs.writeFile('../server/data-json/events.json', eventsDataJSON, callbackFunc)
+fs.writeFile('../server/data-json/ministry-job-openings.json', ministryJobOpeningsJSON, callbackFunc)
 fs.writeFile('../server/data-json/missions.json', missionsDataJSON, callbackFunc)
 fs.writeFile('../server/data-json/our-history.json', ourHistoryDataJSON, callbackFunc)
 fs.writeFile('../server/data-json/our-team.json', ourTeamDataJSON, callbackFunc)
-fs.writeFile('../server/data-json/ministry-job-openings.json', ministryJobOpeningsJSON, callbackFunc)
 fs.writeFile('../server/data-json/outreach.json', outreachDataJSON, callbackFunc)
 fs.writeFile('../server/data-json/services.json', servicesDataJSON, callbackFunc)
 fs.writeFile('../server/data-json/statement-of-faith.json', statementOfFaithDataJSON, callbackFunc)
