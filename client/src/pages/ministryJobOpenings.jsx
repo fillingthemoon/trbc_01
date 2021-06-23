aa24c4c4dc56369aea572c43288775814cf224a8
philemonheng <phil.hsy@outlook.com>
2021-06-23 14:45:56 +0800

    add note in mjo

--- client/src/pages/ministryJobOpenings.jsx
+++ client/src/pages/ministryJobOpenings.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux'
 
 import Banner from '../components-reusable/Banner'
 import SectionsTemplate from '../page-templates/SectionsTemplate'
+import { colorPrimaryOrange, colorLOrange } from '../helper-files/colors'
 
 import { getWord } from '../helper-files/translate'
 
@@ -10,6 +11,18 @@ import { ministryJobOpenings } from '../helper-files/images'
 const { desk } = ministryJobOpenings
 
 import { getMinistryJobOpenings } from '../reducers/ministryJobOpeningsReducer'
+
+import { Typography } from 'antd'
+const { Paragraph, Text } = Typography
+
+const statementStyle = {
+  fontSize: '1rem',
+  lineHeight: '2rem',
+  textAlign: 'justify',
+  padding: '2rem 10%',
+  color: colorPrimaryOrange,
+  backgroundColor: colorLOrange,
+}
 
 const MinistryJobOpenings = () => {
   const dispatch = useDispatch()
@@ -37,6 +50,20 @@ const MinistryJobOpenings = () => {
           },
         ]}
       />
+      <div style={statementStyle}>
+        <Paragraph style={{ margin: 0 }}>
+          Interested applicants should send a cover letter
+          and CV to&nbsp;
+          <a href="mailto:hr@trbc.org.sg">hr@trbc.org.sg</a> with the subject
+          Application for &quot;(name of position)&quot;. Alternatively, please
+          send your physical application addressed to the same committee, to the
+          church at 45 Thomson Road, Singapore 307584.
+        </Paragraph>
+        <br/>
+        <Paragraph style={{ margin: 0 }}>
+          All applications will be treated with strict confidentiality.
+        </Paragraph>
+      </div>
       <div className="">
         <SectionsTemplate data={ministryJobOpeningsData} displayType="card" />
       </div>
