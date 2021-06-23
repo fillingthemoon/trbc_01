021f3c6e299898af385a5fe599b661ac4911c8ea
philemonheng <phil.hsy@outlook.com>
2021-06-23 11:15:55 +0800

    update jsonifyData with mjo

--- client/src/helper-files/jsonifyData.js
+++ client/src/helper-files/jsonifyData.js
@@ -12,6 +12,7 @@ import eventsData from '../data/events.js'
 import missionsData from '../data/missions.js'
 import ourHistoryData from '../data/our-history.js'
 import ourTeamData from '../data/our-team.js'
+import ministryJobOpeningsData from '../data/ministry-job-openings.js'
 import outreachData from '../data/outreach.js'
 import servicesData from '../data/services.js'
 import statementOfFaithData from '../data/statement-of-faith.js'
@@ -23,6 +24,7 @@ const eventsDataJSON = JSON.stringify(eventsData, null, 2)
 const missionsDataJSON = JSON.stringify(missionsData, null, 2)
 const ourHistoryDataJSON = JSON.stringify(ourHistoryData, null, 2)
 const ourTeamDataJSON = JSON.stringify(ourTeamData, null, 2)
+const ministryJobOpeningsJSON = JSON.stringify(ministryJobOpeningsData, null, 2)
 const outreachDataJSON = JSON.stringify(outreachData, null, 2)
 const servicesDataJSON = JSON.stringify(servicesData, null, 2)
 const statementOfFaithDataJSON = JSON.stringify(statementOfFaithData, null, 2)
@@ -41,6 +43,7 @@ fs.writeFile('../server/data-json/events.json', eventsDataJSON, callbackFunc)
 fs.writeFile('../server/data-json/missions.json', missionsDataJSON, callbackFunc)
 fs.writeFile('../server/data-json/our-history.json', ourHistoryDataJSON, callbackFunc)
 fs.writeFile('../server/data-json/our-team.json', ourTeamDataJSON, callbackFunc)
+fs.writeFile('../server/data-json/ministry-job-openings.json', ministryJobOpeningsJSON, callbackFunc)
 fs.writeFile('../server/data-json/outreach.json', outreachDataJSON, callbackFunc)
 fs.writeFile('../server/data-json/services.json', servicesDataJSON, callbackFunc)
 fs.writeFile('../server/data-json/statement-of-faith.json', statementOfFaithDataJSON, callbackFunc)
