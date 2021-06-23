2db33c7e5ab17a5a5edaf316be35fe5020ffe234
philemonheng <phil.hsy@outlook.com>
2021-06-23 16:17:37 +0800

    organise inline styles

--- client/src/components-reusable/ServiceCard.jsx
+++ client/src/components-reusable/ServiceCard.jsx
@@ -13,33 +13,31 @@ import {
 
 const { Title, Text } = Typography
 
-const imgStyle = {
-  maxHeight: '250px',
-  objectFit: 'cover',
-}
-
-const titleStyle = {
-  fontSize: '1.5rem',
-  textAlign: 'left',
-  margin: '15px',
-}
-
-const detailsStyle = {
-  padding: '10px 20px',
-  backgroundColor: colorLGray,
-}
-
-const detailItemStyle = {
-  display: 'flex',
-  alignItems: 'center',
-  margin: '6px 0',
-  fontWeight: '500',
-}
-
-const iconStyle = {
-  fontSize: '1rem',
-  marginRight: '10px',
-  color: iconColor,
+const styles = {
+  img: {
+    maxHeight: '250px',
+    objectFit: 'cover',
+  },
+  title: {
+    fontSize: '1.5rem',
+    textAlign: 'left',
+    margin: '15px',
+  },
+  details: {
+    padding: '10px 20px',
+    backgroundColor: colorLGray,
+  },
+  detailItem: {
+    display: 'flex',
+    alignItems: 'center',
+    margin: '6px 0',
+    fontWeight: '500',
+  },
+  icon: {
+    fontSize: '1rem',
+    marginRight: '10px',
+    color: iconColor,
+  },
 }
 
 const ServiceCard = ({ serviceItem }) => {
@@ -51,22 +49,22 @@ const ServiceCard = ({ serviceItem }) => {
     // Technically, Services is on the I'm New page not the Services page, but this is an exception
     <Link to={`/${page}/${id}`}>
       <Card
-        cover={<img alt="img" src={imgSrc} style={imgStyle} />}
+        cover={<img alt="img" src={imgSrc} style={styles.img} />}
         hoverable
         className="service-card"
       >
-        <Title style={titleStyle}>{title}</Title>
+        <Title style={styles.title}>{title}</Title>
         {details && (
-          <Col style={detailsStyle}>
+          <Col style={styles.details}>
             {time && (
-              <Row style={detailItemStyle}>
-                <AiFillClockCircle style={iconStyle} />
+              <Row style={styles.detailItem}>
+                <AiFillClockCircle style={styles.icon} />
                 <Text>{time}</Text>
               </Row>
             )}
             {location && (
-              <Row style={detailItemStyle}>
-                <FaMapMarkerAlt style={iconStyle} />
+              <Row style={styles.detailItem}>
+                <FaMapMarkerAlt style={styles.icon} />
                 <Text>{location}</Text>
               </Row>
             )}
