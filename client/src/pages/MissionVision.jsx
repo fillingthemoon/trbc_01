786acb06c8f462d3b9f10967d101e77e11a558e7
philemonheng <phil.hsy@outlook.com>
2021-07-10 14:43:21 +0800

    only english/chinese for mv

--- client/src/pages/MissionVision.jsx
+++ client/src/pages/MissionVision.jsx
@@ -31,21 +31,23 @@ const MissionVision = () => {
         <Col className="mv-text-col">
           <TitleText
             className="mv-text-mission"
-            title="Mission 使命"
-            text={[
-              'To Be Disciples Moving as One Body in Christ, Bearing Fruit and Being the Salt and Light of the World.',
-              '成为在基督里同心合一、多结果子、作盐作光的门徒',
-            ]}
+            title={language === 'en' ? 'Mission' : '使命'}
+            text={
+              language === 'en'
+                ? 'To Be Disciples Moving as One Body in Christ, Bearing Fruit and Being the Salt and Light of the World.'
+                : '成为在基督里同心合一、多结果子、作盐作光的门徒'
+            }
             titleStyle={{ fontSize: '2rem', marginBottom: '30px' }}
             textStyle={{ fontSize: '1rem', margin: '0' }}
           />
           <TitleText
             className="mv-text-mission"
-            title="Vision 异象"
-            text={[
-              'One Church Growing Inwardly and Outwardly.',
-              '一个向内、向外成长的教会',
-            ]}
+            title={language === 'en' ? 'Vision' : '异象'}
+            text={
+              language === 'en'
+                ? 'One Church Growing Inwardly and Outwardly.'
+                : '一个向内、向外成长的教会'
+            }
             titleStyle={{ fontSize: '2rem', marginBottom: '3s0px' }}
             textStyle={{ fontSize: '1rem', margin: '0' }}
           />
