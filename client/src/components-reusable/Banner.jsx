60f1af365e0074bf3744ba064d34ccd71e1024a5
philemonheng <phil.hsy@outlook.com>
2021-09-28 16:06:17 +0800

    remove filter from home banner

--- client/src/components-reusable/Banner.jsx
+++ client/src/components-reusable/Banner.jsx
@@ -7,8 +7,18 @@ const Banner = ({ imageTitlePairs }) => {
     <Carousel autoplay autoplaySpeed={3000} adaptiveHeight={true} accessibility>
       {imageTitlePairs.map(({ image, title, titleFontSize }, i) => (
         <div className="banner-container" key={i}>
-          <img className="banner-exterior" src={image} alt="" />
-          <span style={{ fontSize: titleFontSize }} className="banner-title">{title}</span>
+          <img
+            className={
+              image.includes('general/Home')
+                ? 'banner-exterior'
+                : 'banner-exterior-with-filter'
+            }
+            src={image}
+            alt=""
+          />
+          <span style={{ fontSize: titleFontSize }} className="banner-title">
+            {title}
+          </span>
         </div>
       ))}
     </Carousel>
